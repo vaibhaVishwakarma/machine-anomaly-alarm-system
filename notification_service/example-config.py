46b98e1ae9361f6532e6282d290ce9564fca7b48
vaibhaVishwakarma <vaibhavofficial709@gmail.com>
2026-03-05 11:20:10 +0530

    updated config

---
+++ notification_service/example-config.py
@@ -0,0 +1,9 @@
+KAFKA_BOOTSTRAP = "kafka:29092"
+ALERT_TOPIC = "alert_event_topic"
+BACKEND_URL = "http://host.docker.internal:8000"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USER = "vaibhavjs709@gmail.com"
+EMAIL_PASSWORD = "abcdefghijklmnop"
+
+COOLDOWN_SECONDS = 150  # 150 sec window
